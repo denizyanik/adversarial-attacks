e6890e6b1e8a24458c6a6e3f1d7e34b7c2c011a4
dy1g18 <dy1g18@soton.ac.uk>
2021-03-29 22:47:11 +0100

    fixed batch attack

--- ZOO.py
+++ ZOO.py
@@ -1,3 +1,4 @@
+import gc
 import math
 import sys
 
@@ -29,49 +30,60 @@ def zoo_adam_attack_batch(xs,ys):
     eps = 1e-8
     h = 0.0001
     iterations = 1000
+    coordinates = [0] * batch
 
     # variables to calculate gradient
     variables = np.empty([batch*2,96,173,1])
+    perturbations = np.zeros([batch,96, 173, 1])
 
     for i in tqdm(range(1,iterations+1)):
 
-        for z in range(batch):
-            var = xs[z:z + 1, :, :, :]
+        check = 0
+        for z in range(0,batch*2,2):
+            var = xs[check:check + 1, :, :, :]
             variables[z] = var
             variables[z + 1] = var
+            check += 1
 
-        for j in range(0,batch):
-            size = xs[j:j + 1, :, :, :].size
+        check = 0
+        for j in range(0,batch*2,2):
+            size = xs[check:check + 1, :, :, :].size
 
             # choose a random coordinate and compute partial gradient
-            coordinate = np.random.choice(size, 1, replace=True)[0]
+            coordinates[check] = np.random.choice(size, 1, replace=True)[0]
 
-            variables[j].reshape(-1)[coordinate] += h
-            variables[j+1].reshape(-1)[coordinate] -= h
+            variables[j].reshape(-1)[coordinates[check]] += h
+            variables[j+1].reshape(-1)[coordinates[check]] -= h
+            check += 1
 
         # calculate batch losses of all variables to calculate gradient for each sound file
         losses = get_loss_batch(variables,ys,batch)
 
         # calculate partial gradient
+        check = 0
         for j in range(0,batch):
-            perturbation = np.zeros([96, 173, 1])
 
             x = xs[j:j + 1, :, :, :]
 
             gradient = np.zeros(x.reshape(-1).shape)
-            x1_loss = losses[j]
-            x2_loss = losses[j+1]
-            gradient[coordinate] = (x1_loss - x2_loss / (2 * h))
+            x1_loss = losses[check]
+            x2_loss = losses[check+1]
+
+            gradient[coordinates[j]] = (x1_loss - x2_loss / (2 * h))
 
             mt[j] = beta1 * mt[j] + (1 - beta1) * gradient
             vt[j] = beta2 * vt[j] + (1 - beta2) * np.square(gradient)
             corr = (math.sqrt(1 - beta2 ** i)) / (1 - beta1 ** i)
 
-            m = perturbation.reshape(-1)
+            m = perturbations[j:j + 1, :, :, :].reshape(-1)
             m -= learn_rate * corr * (mt[j] / (np.sqrt(vt[j]) + eps))
-            perturbation = m.reshape(perturbation.shape)
+            perturbations[j:j + 1, :, :, :] = m.reshape(perturbations[j:j + 1, :, :, :].shape)
 
-            xs[j] += perturbation
+            xs[j:j + 1, :, :, :] += perturbations[j:j + 1, :, :, :]
+            check += 2
+
+        del (losses)
+        gc.collect()
 
     return xs
 
@@ -118,9 +130,10 @@ def zoo_adam_attack(x,y):
     batch = 1
     iterations = 1000
 
-    perturbation = np.zeros([1,96,173,1])
+    perturbation = np.zeros([1, 96, 173, 1])
 
     for i in range(1,iterations+1):
+
         x1 = x
         x2 = x
 
@@ -147,7 +160,7 @@ def zoo_adam_attack(x,y):
 
         x += perturbation
 
-    return x
+    return (x)
 
 
 def get_loss(x,y):
@@ -175,7 +188,7 @@ correct = 0
 r_correct = 0
 
 # test accuracy
-'''
+
 for i in tqdm(range(0,X_test.shape[0])):
     r_check = model.predict(X_test[i:i + 1, :, :, :])
     r_prediction = decode_class(r_check, class_names)
@@ -195,14 +208,12 @@ for i in tqdm(range(0,X_test.shape[0])):
 
     print("current adversarial accuracy is "+ str((correct/total)*100) + "%")
     print("current accuracy is "+ str((r_correct/total)*100) + "%")
-'''
+
 
 # test batch accuracy
 
 adversarials = zoo_adam_attack_batch(X_test,Y_test)
 for i in range(0,adversarials.shape[0]):
-    real_check = model.predict(X_test[i:i + 1, :, :, :])
-    real_prediction = decode_class(real_check, class_names)
 
     adversarial_example = adversarials[i:i+1,:,:,:]
     adversarial_example = model.predict(adversarial_example)
@@ -214,11 +225,8 @@ for i in range(0,adversarials.shape[0]):
 
     if (prediction == real):
         correct += 1
-    if (real_prediction == real):
-        r_correct += 1
 
     print("current adversarial accuracy is " + str((correct / total) * 100) + "%")
-    print("current accuracy is " + str((r_correct / total) * 100) + "%")
 
 
 # mean squared error
